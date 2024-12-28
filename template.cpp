6a942c7b5d6428638170191417f369c75ed79929
Priashis  Ghosh <121686927+PriashisG@users.noreply.github.com>
2024-12-28 17:13:32 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Type Names
+// Typedef
 typedef unsigned long long ull;
 typedef long long ll;
-typedef double dl;
-typedef vector <int> vi;
-typedef vector <ll> vl;
-typedef vector <vi> vvi;
-typedef vector <vl> vvl;
-typedef pair <int, int> pii;
-typedef pair <double, double> pdd;
-typedef pair <ll, ll> pll;
-typedef vector <pii> vii;
-typedef vector <pll> vll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
 
-// Macros
+// Macos
 #define PB push_back
 #define all(x) x.begin(), x.end()
 #define trav(i, a) for (auto i : a)
@@ -25,91 +23,24 @@ typedef vector <pll> vll;
 #define S second
 #define endl '\n'
 #define sz(x) (int)x.size()
-#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
-#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 
-// DEBUGGER
-template < typename F, typename S >
-ostream& operator << ( ostream& os, const pair< F, S > & p ) {
-    return os << "(" << p.first << ", " << p.second << ")";
-}
- 
-template < typename T >
-ostream &operator << ( ostream & os, const vector< T > &v ) {
-    os << "{";
-        for(auto it = v.begin(); it != v.end(); ++it) {
-			if( it != v.begin() ) os << ", ";
-			os << *it;
-			}
-		return os << "}";
-}
- 
-template < typename T >
-ostream &operator << ( ostream & os, const set< T > &v ) {
-	os << "[";
-		for(auto it = v.begin(); it != v.end(); ++it) {
-			if( it != v.begin() ) os << ", ";
-			os << *it;
-			}
-		return os << "]";
-}
- 
-template < typename T >
-ostream &operator << ( ostream & os, const multiset< T > &v ) {
-	os << "[";
-		for(auto it = v.begin(); it != v.end(); ++it) {
-			if( it != v.begin() ) os << ", ";
-			os << *it;
-			}
-		return os << "]";
-}
- 
-template < typename F, typename S >
-ostream &operator << ( ostream & os, const map< F, S > &v ) {
-	os << "[";
-		for(auto it = v.begin(); it != v.end(); ++it) {
-			if( it != v.begin() ) os << ", ";
-			os << it -> first << " = " << it -> second ;
-			}
-		return os << "]";
-}
- 
-#define dbg(args...) do {cerr << #args << " : "; func(args); } while(0)
- 
-void func () {
-	cerr << endl;
-}
- 
-template <typename T>
-void func( T a[], int n ) {
-	for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
-	cerr << endl;
-}
- 
-template <typename T, typename ... hello>
-void func( T arg, const hello &... rest) {
-	cerr << arg << ' ';
-	func(rest...);
-}
+// Constant
+const ll MOD = 1e9 + 7;
 
-void fast() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-ll LCM(ll a, ll b){
-    return (a*b)/ GCD(a,b);
+void fast(){
+	ios::sync_with_stdio(false);
+    cin.tie(0);
 }
 
 void solve() {
-	return;
+	
 }
 
-
 int main(void) {
-    fast();
+	fast();
 
     int t;
     cin >> t;
-    while (t--) solve();
+    while (t--) 
+        solve();
 }
